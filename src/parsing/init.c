73b30f7db290f920f09f7d0316bb2cd4bc7205c1
Antoine Allou Vincheneux <aallou-v@c1r6p9.42lehavre.fr>
2024-06-03 19:35:42 +0200

    Patch Parsing Data

--- src/parsing/init.c
+++ src/parsing/init.c
@@ -6,7 +6,7 @@
 /*   By: aallou-v <aallou-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 18:24:39 by aallou-v          #+#    #+#             */
-/*   Updated: 2024/06/03 19:17:22 by aallou-v         ###   ########.fr       */
+/*   Updated: 2024/06/03 19:32:59 by aallou-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,6 +65,12 @@ static int	init_var(t_core *core)
 	core->img->path_west = NULL;
 	core->map->screen_height = SIZE_HEIGHT;
 	core->map->screen_width = SIZE_WIDTH;
+	core->img->celling->r = -1;
+	core->img->celling->g = -1;
+	core->img->celling->b = -1;
+	core->img->floor->r = -1;
+	core->img->floor->g = -1;
+	core->img->floor->b = -1;
 	core->pos[X] = -1;
 	core->pos[Y] = -1;
 	if (core->map == NULL || core->img == NULL || core->img->celling == NULL
